569a5b24940b6dd6c70cc8efe1c01034ef3b67ce
tatijan <34741441+tatijan@users.noreply.github.com>
2018-02-21 12:10:29 +0200

    Add files via upload

---
+++ Address.php
@@ -0,0 +1,15 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+class Address
+{
+    public function findCoords($address)
+    {
+        $api = new \Yandex\Geo\Api;
+        $api->setQuery($address);
+        $api
+            ->setLang(\Yandex\Geo\Api::LANG_RU) // локаль ответа
+            ->load();
+        $response = $api->getResponse();
+        return $response->getList();
+    }
+}
